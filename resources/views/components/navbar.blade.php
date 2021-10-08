e8f10338375f0c6aef27ddf2ed9933768f0f9231
Suhail Najmi <amocuk@gmail.com>
2021-10-09 02:16:21 +0800

    Writing

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -15,14 +15,6 @@
         </div>
     </div>
     <div class="navbar-end">
-        <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                class="inline-block w-6 h-6 stroke-current">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9">
-                </path>
-            </svg>
-        </button>
         <div class="dropdown dropdown-end">
             {{-- <div tabindex="0" class="m-1 btn"> --}}
             <div tabindex="0">
@@ -34,7 +26,60 @@
                     </div>
                 </div>
             </div>
-            <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+
+
+            <ul tabindex="0" class="w-52 menu py-3 shadow-lg bg-base-100 rounded-box dropdown-content">
+                <li class="menu-title">
+                    <span>
+                        Menu Title
+                    </span>
+                </li>
+                <li>
+                    <a>
+                        Item without icon
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="inline-block w-5 h-5 mr-2 stroke-current">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
+                            </path>
+                        </svg>
+                        Item with icon
+    
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="inline-block w-5 h-5 mr-2 stroke-current">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
+                        </svg>
+                        Item with icon
+    
+                        <div class="badge ml-2 success">3</div>
+                    </a>
+                </li>
+                <li>
+                    {{-- <input type="submit" value="submit"> --}}
+                    <a href="/logout" onclick="event.preventDefault(); document.forms['logout'].submit();">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-5 h-5 mr-2 stroke-current" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                        </svg>
+                        Log Keluar
+                    </a>
+                    <form name="logout" action="/logout" method="POST" class="hidden">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+{{-- 
+            <ul class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
                 <li>
                     <a>Item 1</a>
                 </li>
@@ -43,12 +88,13 @@
                 </li>
                 <div class="divider"></div>
                 <li>
+
                     <form action="/logout" method="POST" class="px-5 py-3">
                         @csrf
                         <button>Logout</button>
                     </form>
                 </li>
-            </ul>
+            </ul> --}}
         </div>
 
     </div>
