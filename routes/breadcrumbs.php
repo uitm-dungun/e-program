8b39cf19bcc435289bc36cce59d29599c9121913
Mysticmirror02 <91043810+Mysticmirror02@users.noreply.github.com>
2022-02-16 17:00:06 +0800

    breadcrumb for user role

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -4,32 +4,42 @@ use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
 
 // Index
-
+// Login > CreationIndex
 Breadcrumbs::for('paperwork.creation.index', function(BreadcrumbTrail $trail) {
     $trail->push('E-Paperwork', route('paperwork.creation.index'));
 });
-
-Breadcrumbs::for('paperwork.creation.show', function ($trail) {
-    $trail->push('Title Here', route('paperwork.creation.show'));
+// Login > CreationIndex > CreationShow
+Breadcrumbs::for('paperwork.creation.show', function (g$trail) {
+    $trail->parent('paperwork.creation.index');
+    $trail->push('E-Paperwork', route('paperwork.creation.show'));
 });
+// Login > CreationIndex > CreationCreate[permohonan]
 
+// Login > ValidationIndex
 Breadcrumbs::for('paperwork.validation.index', function ($trail) {
     $trail->push('E-Paperwork', route('paperwork.validation.index'));
 });
 
+// Login > ValidationIndex > ValidationShow
+Breadcrumbs::for('paperwork.validation.show', function ($trail) {
+    $trail->parent('paperwork.validation.index');
+    $trail->push('Pengesahan', route('paperwork.validation.show', 1));
+});
+
+
+// Login > ValidationIndex > ValidationCreate [Pengesahan KK]
+
+// Login > SupportIndex
 Breadcrumbs::for('paperwork.support.index', function ($trail) {
     $trail->push('E-Paperwork', route('paperwork.support.index'));
 });
-
+// Login > SupportIndex > SupportShow [P2engesahan KK]
 Breadcrumbs::for('paperwork.support.show', function ($trail) {
     $trail->parent('paperwork.support.index');
     $trail->push('Sokongan Kertas Kerja', route('paperwork.support.show', '2'));
 });
 
-Breadcrumbs::for('paperwork.validation.show', function ($trail) {
-    $trail->parent('paperwork.validation.index');
-    $trail->push('Pengesahan', route('paperwork.validation.show', 1));
-});
+
 
 // // Create
 // Breadcrumbs::for('paperwork.phase-1.create', function (BreadcrumbTrail $trail) {
